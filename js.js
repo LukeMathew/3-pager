97f3d5ae931bb764fae0d3b9dd87ef739a449827
Astrocat2374 <sugiyama.evan@gmail.com>
2020-03-04 09:57:48 -1000

    first commit

---
+++ js.js
@@ -0,0 +1,24 @@
+function pencil(){
+document.body.style.backgroundImage="url('https://media1.giphy.com/media/xThtagXth2JHVJ7MY0/source.gif')";
+};
+function pp(){
+document.body.style.backgroundImage="url('https://thumbs.gfycat.com/InbornBitterAllensbigearedbat-max-1mb.gif')";
+};
+function ppp(){
+document.body.style.backgroundImage="url('https://media3.giphy.com/media/WS6CDvvyNDrhZRFBtT/giphy.gif')";
+};
+function c(){
+document.body.style.backgroundImage="url('https://media3.gihy.com/media/WS6CDvvyNDrhZRFBtT/giphy.gif')";
+};
+document.getElementById("pencil").onclick = function() {
+  pencil();
+};
+document.getElementById("pp").onclick = function() {
+  pp();
+};
+document.getElementById("ppp").onclick = function() {
+  ppp();
+};
+document.getElementById("c").onclick = function() {
+  c();
+};
